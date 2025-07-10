6ffee7658b118d1fad1369e87449c4a22bc0857f
anhquan2102 <quantahe170941@fpt.edu.vn>
2025-07-10 16:16:56 +0700

    update

--- app/src/main/java/com/prm392/foodmap/models/User.java
+++ app/src/main/java/com/prm392/foodmap/models/User.java
@@ -40,4 +40,16 @@ public class User {
     public String getUid() {
         return uid;
     }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public long getCreatedAt() { return createdAt; }
+    public void setCreatedAt(long createdAt) { this.createdAt = createdAt; }
 }
